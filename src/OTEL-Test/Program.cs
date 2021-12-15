34829c2f6ea97b53397b9b32b715c5f4570041d9
Andreas Vu <andreas.vu@arvato.com>
2021-12-15 15:01:38 +0100

    first commit

---
+++ src/OTEL-Test/Program.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using OpenTelemetry.Logs;
+using OpenTelemetry.Metrics;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
+using OTEL_Test;
+
+AppContext.SetSwitch("System.Net.Http.SocketsHttpHandler.Http2UnencryptedSupport", true);
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Configure metrics
+builder.Services.AddOpenTelemetryMetrics(builder =>
+{
+    builder.SetResourceBuilder(ResourceBuilder.CreateDefault().AddTelemetrySdk().AddEnvironmentVariableDetector());
+    builder.AddHttpClientInstrumentation();
+    builder.AddAspNetCoreInstrumentation();
+    builder.AddMeter("*");
+    builder.AddOtlpExporter(options => options.Endpoint = new Uri("http://localhost:4317"));
+});
+
+// Configure tracing
+builder.Services.AddOpenTelemetryTracing(builder =>
+{
+    builder.SetResourceBuilder(ResourceBuilder.CreateDefault().AddTelemetrySdk().AddEnvironmentVariableDetector());
+    builder.AddHttpClientInstrumentation();
+    builder.AddAspNetCoreInstrumentation();
+    builder.AddSource("*");
+    builder.AddOtlpExporter(options => options.Endpoint = new Uri("http://localhost:4317"));
+});
+
+// Configure logging
+builder.Logging.AddOpenTelemetry(builder =>
+{
+    builder.SetResourceBuilder(ResourceBuilder.CreateDefault().AddTelemetrySdk().AddEnvironmentVariableDetector());
+    builder.IncludeFormattedMessage = true;
+    builder.IncludeScopes = true;
+    builder.ParseStateValues = true;
+    builder.AddOtlpExporter(options => options.Endpoint = new Uri("http://otel-collector:4317"));
+});
+
+builder.Services.AddHostedService<Worker>();
+
+var app = builder.Build();
+app.Run();
